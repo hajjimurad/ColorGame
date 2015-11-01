5479f7a1046a3cfba0b226acd6a334812edec9e7
murad <muradm@bk.ru>
2015-11-01 22:48:00 +0100

    view model with cells added

---
+++ scripts/GameViewModel.js
@@ -0,0 +1,66 @@
+/**
+ * Created by murad on 18/10/15.
+ */
+
+requirejs(["lib/knockout", "Board", "Cell", "StrategySimple", "BoardGenerator"], function (ko, Board, Cell, StrategySimple, BoardGenerator) {
+
+    var availableCellsColors = [
+        "FF0000", "00FF00", "0000FF", "FFFF00", "FF00FF", "00FFFF", "000000",
+        "800000", "008000", "000080", "808000", "800080", "008080", "808080",
+        "C00000", "00C000", "0000C0", "C0C000", "C000C0", "00C0C0", "C0C0C0",
+        "400000", "004000", "000040", "404000", "400040", "004040", "404040",
+        "200000", "002000", "000020", "202000", "200020", "002020", "202020",
+        "600000", "006000", "000060", "606000", "600060", "006060", "606060",
+        "A00000", "00A000", "0000A0", "A0A000", "A000A0", "00A0A0", "A0A0A0",
+        "E00000", "00E000", "0000E0", "E0E000", "E000E0", "00E0E0", "E0E0E0"
+    ];
+
+    var getColorStyle = function (colorIndex) {
+        return "#" + availableCellsColors[colorIndex];
+    };
+
+    var updateViewModelColors = function () {
+        for (var index in cellsColors) {
+            var cellDestination = cellsColors[index];
+            var cellSource = cells[index];
+            cellDestination(getColorStyle(cellSource.getColor()));
+        }
+    };
+
+    var boardGenerator = new BoardGenerator(10, 10);
+    var board = boardGenerator.generate();
+
+    var strategy = new StrategySimple(board, {x: 0, y: 0});
+    var stepNumber = ko.observable(0);
+
+    var cellsColors = [];
+    var cells = board.getCells();
+    var cellCount = cells.length;
+    for (var i = 0; i < cellCount; i++) {
+        cellsColors.push(ko.observable());
+    }
+    
+    updateViewModelColors();
+
+
+    var viewModel = {
+        getCells: function () {
+            return cellsColors;
+        },
+        nextStep: function () {
+            strategy.nextStep();
+            stepNumber(strategy.getStepNumber());
+
+            updateViewModelColors();
+        },
+        getSize: function () {
+            return board.size;
+        },
+        getStepNumber: function () {
+            return stepNumber();
+        }
+    };
+
+    ko.applyBindings(viewModel);
+
+});
