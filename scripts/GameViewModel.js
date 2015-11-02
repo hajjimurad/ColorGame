bd8f257a5a2bc1a7bf81e95bbbd614d6dcb0ec15
murad <muradm@bk.ru>
2015-11-02 22:21:43 +0100

    clean up model

--- scripts/GameViewModel.js
+++ scripts/GameViewModel.js
@@ -4,25 +4,7 @@
 
 requirejs(["lib/knockout", "Board", "Cell", "StrategySimple", "BoardGenerator"], function (ko, Board, Cell, StrategySimple, BoardGenerator) {
 
-    var boardGenerator = new BoardGenerator(10, 10);
-    var board = boardGenerator.generate();
-    var cells = board.getCells();
-    var strategy = new StrategySimple(board, {x: 0, y: 0});
-
-    var self = this;
-
-    self.stepNumber = ko.observable(0);
-    self.cellsColors = [];
-    self.boardSize = ko.observable(board.size);
-
-    self.nextStep = function () {
-        strategy.nextStep();
-        self.stepNumber(strategy.getStepNumber());
-
-        updateViewModelColors();
-    };
-
-    var availableCellsColors = [
+    GameViewModel.availableCellsColors = [
         "FF0000", "00FF00", "0000FF", "FFFF00", "FF00FF", "00FFFF", "000000",
         "800000", "008000", "000080", "808000", "800080", "008080", "808080",
         "C00000", "00C000", "0000C0", "C0C000", "C000C0", "00C0C0", "C0C0C0",
@@ -33,32 +15,53 @@ requirejs(["lib/knockout", "Board", "Cell", "StrategySimple", "BoardGenerator"],
         "E00000", "00E000", "0000E0", "E0E000", "E000E0", "00E0E0", "E0E0E0"
     ];
 
-    var getCellColorStyle = function (colorIndex) {
-        return "#" + availableCellsColors[colorIndex];
+    function GameViewModel() {
+        var self = this;
+
+        var boardGenerator = new BoardGenerator(10, 10);
+        var board = boardGenerator.generate();
+        var cells = board.getCells();
+        var strategy = new StrategySimple(board, {x: 0, y: 0});
+
+        self.stepNumber = ko.observable(0);
+        self.cellsColors = [];
+        self.boardSize = ko.observable(board.size);
+
+        self.nextStep = function () {
+            strategy.nextStep();
+            self.stepNumber(strategy.getStepNumber());
+
+            updateViewModelColors();
+        };
+
+
+        var getCellColorStyle = function (colorIndex) {
+            return "#" + GameViewModel.availableCellsColors[colorIndex];
+        };
+
+        var initViewModelColors = function () {
+            var cellCount = cells.length;
+            for (var i = 0; i < cellCount; i++) {
+                self.cellsColors.push({
+                    color: ko.observable(),
+                    colorIndex: ko.observable()
+                });
+            }
+        };
+
+        var updateViewModelColors = function () {
+            for (var index in self.cellsColors) {
+                var cellDestination = self.cellsColors[index];
+                var cellSource = cells[index];
+
+                cellDestination.color(getCellColorStyle(cellSource.getColor()));
+                cellDestination.colorIndex(cellSource.getColor());
+            }
+        };
+
+        initViewModelColors();
+        updateViewModelColors();
     };
 
-    var initViewModelColors = function () {
-        var cellCount = cells.length;
-        for (var i = 0; i < cellCount; i++) {
-            cellsColors.push({
-                color: ko.observable(),
-                colorIndex: ko.observable()
-            });
-        }
-    };
-
-    var updateViewModelColors = function () {
-        for (var index in self.cellsColors) {
-            var cellDestination = self.cellsColors[index];
-            var cellSource = cells[index];
-
-            cellDestination.color(getCellColorStyle(cellSource.getColor()));
-            cellDestination.colorIndex(cellSource.getColor());
-        }
-    };
-
-    initViewModelColors();
-    updateViewModelColors();
-
-    ko.applyBindings(self);
+    ko.applyBindings(new GameViewModel());
 });
